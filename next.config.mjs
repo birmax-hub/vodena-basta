8a3f39eec189bf4c5d2460b4d014b3a5d6dc536c
Srdjan Radin <birmax@gmail.com>
2025-11-22 14:17:55 +0100

    seo optimise

--- next.config.mjs
+++ next.config.mjs
@@ -37,15 +37,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
         source: '/_next/static/:path*',
         headers: [
           {
